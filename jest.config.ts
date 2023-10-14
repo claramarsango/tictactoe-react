ed360a81654364d1a8c021cc13763dc0696e4913
Clara Marsango <claraxmarsango@gmail.com>
2023-10-15 00:08:20 +0200

    fix final new line and indentation formatting

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 /**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
+  For a detailed explanation regarding each configuration property, visit:
+  https://jestjs.io/docs/configuration
  */
 
 import type { Config } from 'jest';
